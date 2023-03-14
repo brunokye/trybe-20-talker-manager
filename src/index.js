73887d21cb46644315c9c30d54b63e2b529114c5
Bruno Ferreira <contatobrunokye@gmail.com>
2023-03-14 15:41:12 -0300

    feat: requisito 03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const talkerRoutes = require('./routes/talkerRoutes');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,17 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/talker', talkerRoutes);
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if ([email, password].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const newToken = generateToken();
+  res.status(200).json({ token: newToken });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
